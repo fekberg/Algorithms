d5e9c285ade56205d74e5053fa2e67252b73296a
Filip Ekberg <mail@filipekberg.se>
2014-02-08 19:15:38 +1100

    Peak Finding
    Added the first algorithm which is peak finding. It's a visual studio
    2013 project so you can open it and just run it. There is no
    documentation in the code, so head over to the MIT course and have a
    look.

---
+++ Peak Finding/Peak Finding/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peak_Finding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] problem = new[]{
+		        new [] {0,  0,  9,  0,  0,  0,  0},
+		        new [] {0,  0,  0,  0,  0,  0,  0},
+		        new [] {0,  1,  0,  0,  0,  0,  0},
+		        new [] {0,  2,  0,  0,  0,  0,  0},
+		        new [] {0,  0,  0,  0,  0,  0,  0},
+		        new [] {0,  0,  0,  0,  0,  0,  0},
+		        new [] {0,  0,  0,  0,  0,  0,  0},
+	        };
+
+            Console.WriteLine("Found a peak with value	: {0}", FindMax(problem));
+        }
+
+
+        static int FindMax(int[][] problem, int j = -1)
+        {
+            if (problem.Length <= 0) return 0;
+
+            if (j == -1) j = problem.Length / 2;
+            int globalMax = FindGlobalMax(problem, j);
+
+            if (
+                (globalMax - 1 > 0 &&
+                problem[globalMax][j] >=
+                problem[globalMax - 1][j]) &&
+
+                problem[globalMax][j] >=
+                problem[globalMax + 1][j] &&
+
+                (j - 1 > 0 &&
+                problem[globalMax][j] >=
+                problem[globalMax][j - 1]) &&
+
+                problem[globalMax][j] >=
+                problem[globalMax][j + 1]
+                )
+            {
+                return problem[globalMax][j];
+            }
+            else if (problem[globalMax][j - 1] > problem[globalMax][j])
+            {
+                return FindMax(problem, j / 2);
+            }
+            else if (problem[globalMax][j + 1] > problem[globalMax][j])
+            {
+                return FindMax(problem, problem.Length - (j / 2));
+            }
+
+            return problem[globalMax][j];
+        }
+
+        static int FindGlobalMax(int[][] problem, int column)
+        {
+            int max = problem[0][column];
+            int index = 0;
+            for (int i = 0; i < problem.Length; i++)
+            {
+                if (max < problem[i][column])
+                {
+                    max = problem[i][column];
+                    index = i;
+                }
+            }
+
+            return index;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-Algorithms
+Algorithms &amp; Data structures
 ==========
 
-Algorithms &amp; Data structures
+I'm reading up on some algorithms and data structures because it's fun and challenging. Some people go to the gym, I tackle algorithms instead.
+
+There's a great resource over at MIT Open Courseware (6.006) that talks about the algorithms in this repository, go and have a look if you fancy it.
+
+http://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-006-introduction-to-algorithms-fall-2011/lecture-videos/lecture-1-algorithmic-thinking-peak-finding/
+
+
+# Running the code
+I write most of the code on paper, then I enter it into LINQPad to find out what kind of errors my on-paper code got. I write most of the code in C#.
